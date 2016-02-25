8a0aad16f7cc55e49d00b37ea799267cd8330ae9
Hasitha Jayanandana <hasithajayanandana@gmail.com>
2016-02-26 02:39:21 +1100

    Disabled API contectivity due to bug

--- floodwatch-combined/src/floodwatch-combined.c
+++ floodwatch-combined/src/floodwatch-combined.c
@@ -349,14 +349,14 @@ static void init(void) {
   });
     window_stack_push(main_window, true);
 
-    app_message_register_inbox_received(inbox_received_callback);
+    /*app_message_register_inbox_received(inbox_received_callback);
     app_message_register_inbox_dropped(inbox_dropped_callback);
     app_message_register_outbox_failed(outbox_failed_callback);
     app_message_register_outbox_sent(outbox_sent_callback);
 
       // Open AppMessage
     app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-
+*/
 
 }
 
