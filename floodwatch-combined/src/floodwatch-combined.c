97c6b058354933d5be8a2e678408668e1f9ae5af
Hasitha Jayanandana <hasithajayanandana@gmail.com>
2016-03-07 23:50:50 +1100

    Added API Functionality with seperated reports

--- floodwatch-combined/src/floodwatch-combined.c
+++ floodwatch-combined/src/floodwatch-combined.c
@@ -17,7 +17,7 @@ typedef struct {
   char *description;
 } Floodinfo;
 
-static Floodinfo flood_info[] = {
+static Floodinfo flood_info[256] = {
 };
 
 // Define the App elemments for the Main Window and Report window
@@ -27,20 +27,18 @@ static MenuLayer *menu_layer;
 static TextLayer *s_weather_layer;
 static TextLayer *contact_layer;
 
+// Create variable for weather layer
+static char weather_layer_buffer[32];
 
 //Create variable to store the index of the currenly selected menu item
 static int current_icon = 0;
 static int floodcount = 0;
-// Store incoming information
-static char area_buffer[32];
-static char source_buffer[32];
-static char weather_layer_buffer[32];
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
   static char area_buffer[32];
   static char source_buffer[32];
-  static char weather_layer_buffer[32];
+
   char *area_temp;
   char *source_temp;
   const char delim[2] = ",";
@@ -64,26 +62,29 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n",area_buffer,source_buffer);
 
     /* get the first token */
-      area_temp = strtok(area_buffer, delim);
+    area_temp = strtok(area_buffer, delim);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",area_temp);
 
-      /* walk through other tokens */
-      while( area_temp != NULL )
-      {
-         flood_info[token_count].area= area_temp;
-         token_count++;
-         area_temp = strtok(NULL, delim);
-      }
+    /* walk through other tokens */
+    while( area_temp != NULL )
+    {
+      flood_info[token_count].area= area_temp;
+      token_count++;
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",flood_info[token_count].area);
+      area_temp = strtok(NULL, delim);
+    }
 
-      token_count = 0;
-      source_temp = strtok(source_buffer, delim);
+    token_count = 0;
+    source_temp = strtok(source_buffer, delim);
 
-      /* walk through other tokens */
-      while( source_temp != NULL )
-      {
-         flood_info[token_count].area= source_temp;
-       token_count++;
-         source_temp = strtok(NULL, delim);
-      }
+    /* walk through other tokens */
+    while( source_temp != NULL )
+    {
+      flood_info[token_count].source= source_temp;
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",flood_info[token_count].source);
+      token_count++;
+      source_temp = strtok(NULL, delim);
+    }
 
   }
 }
@@ -136,17 +137,17 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
   switch (cell_index ->section){
     case 0 :
     // use row to specify hich item to draw
-      switch (cell_index->row){
-        case 0:
-          menu_cell_basic_draw(ctx, cell_layer, "Floods In Area","",NULL);
-          break;
-        case 1:
-          menu_cell_basic_draw(ctx, cell_layer, "Emergency Contacts", "SOS", NULL);
-          break;
-        case 2:
-            menu_cell_basic_draw(ctx, cell_layer, "Configure App", "Select", NULL);
-          break;
-      }
+    switch (cell_index->row){
+      case 0:
+      menu_cell_basic_draw(ctx, cell_layer, "Floods In Area","",NULL);
+      break;
+      case 1:
+      menu_cell_basic_draw(ctx, cell_layer, "Emergency Contacts", "SOS", NULL);
+      break;
+      case 2:
+      menu_cell_basic_draw(ctx, cell_layer, "Configure App", "Select", NULL);
+      break;
+    }
 
   }
 }
@@ -154,17 +155,17 @@ static void select_click(struct MenuLayer *menu_layer, MenuIndex *cell_index, vo
   current_icon = cell_index->row;
   switch (cell_index->row){
     case 0:
-      vibes_short_pulse();
-      window_stack_push(report_window, true);
-      break;
+    vibes_short_pulse();
+    window_stack_push(report_window, true);
+    break;
     case 1:
-      vibes_short_pulse();
-      window_stack_push(contact_window, true);
-      break;
+    vibes_short_pulse();
+    window_stack_push(contact_window, true);
+    break;
     case 2:
-      vibes_short_pulse();
-      break;
-    }
+    vibes_short_pulse();
+    break;
+  }
 
 
 }
@@ -174,10 +175,10 @@ static int16_t get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *cell_i
   if (menu_layer_is_index_selected(menu_layer, cell_index)) {
     switch (cell_index->row) {
       case 0:
-        return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
-        break;
+      return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
+      break;
       default:
-        return MENU_CELL_ROUND_FOCUSED_TALL_CELL_HEIGHT;
+      return MENU_CELL_ROUND_FOCUSED_TALL_CELL_HEIGHT;
     }
   } else {
     return MENU_CELL_ROUND_UNFOCUSED_SHORT_CELL_HEIGHT;
@@ -193,175 +194,175 @@ static void main_window_load(Window *window) {
 
   title_layer = text_layer_create(
     GRect(0, 0,window_bounds.size.w, window_bounds.size.h));
-  text_layer_set_text_alignment(title_layer, GTextAlignmentCenter);
-  text_layer_set_text(title_layer, "FloodWatch");
-  text_layer_set_text_color(title_layer, GColorBlack);
-  text_layer_set_background_color(title_layer, GColorClear);
-  text_layer_set_font(title_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-  layer_add_child(window_layer, text_layer_get_layer(title_layer));
+    text_layer_set_text_alignment(title_layer, GTextAlignmentCenter);
+    text_layer_set_text(title_layer, "FloodWatch");
+    text_layer_set_text_color(title_layer, GColorBlack);
+    text_layer_set_background_color(title_layer, GColorClear);
+    text_layer_set_font(title_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+    layer_add_child(window_layer, text_layer_get_layer(title_layer));
 
-  region_layer = text_layer_create(
-    GRect(0, 20,window_bounds.size.w, window_bounds.size.h));
-  text_layer_set_text_alignment(region_layer, GTextAlignmentCenter);
-  text_layer_set_text(region_layer, "Jakarta");
-  text_layer_set_text_color(region_layer, GColorWhite);
-  text_layer_set_background_color(region_layer, GColorClear);
-  text_layer_set_font(region_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  layer_add_child(window_layer, text_layer_get_layer(region_layer));
-
-
-  menu_layer = menu_layer_create(GRect(0, 50,window_bounds.size.w, window_bounds.size.h));
-  //menu_layer_set_normal_colors(MenuLayer * menu_layer, GColorPictonBlue, GColorBlack);
-  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_sections = menu_get_num_sections_callback,
-    .get_num_rows = menu_get_num_rows_callback,
-    .get_header_height = PBL_IF_RECT_ELSE(menu_get_header_height_callback, NULL),
-    .draw_header = PBL_IF_RECT_ELSE(menu_draw_header_callback, NULL),
-    .draw_row = menu_draw_row_callback,
-    .select_click = select_click,
-    .get_cell_height = PBL_IF_ROUND_ELSE(get_cell_height_callback, NULL),
-  });
-
-  // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(menu_layer, window);
-
-  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
-}
+    region_layer = text_layer_create(
+      GRect(0, 20,window_bounds.size.w, window_bounds.size.h));
+      text_layer_set_text_alignment(region_layer, GTextAlignmentCenter);
+      text_layer_set_text(region_layer, "Jakarta");
+      text_layer_set_text_color(region_layer, GColorWhite);
+      text_layer_set_background_color(region_layer, GColorClear);
+      text_layer_set_font(region_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+      layer_add_child(window_layer, text_layer_get_layer(region_layer));
 
 
-static void main_window_unload(Window *window) {
-  text_layer_destroy(title_layer);
-  text_layer_destroy(region_layer);
-  menu_layer_destroy(menu_layer);
-  }
+      menu_layer = menu_layer_create(GRect(0, 50,window_bounds.size.w, window_bounds.size.h));
+      //menu_layer_set_normal_colors(MenuLayer * menu_layer, GColorPictonBlue, GColorBlack);
+      menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
+        .get_num_sections = menu_get_num_sections_callback,
+        .get_num_rows = menu_get_num_rows_callback,
+        .get_header_height = PBL_IF_RECT_ELSE(menu_get_header_height_callback, NULL),
+        .draw_header = PBL_IF_RECT_ELSE(menu_draw_header_callback, NULL),
+        .draw_row = menu_draw_row_callback,
+        .select_click = select_click,
+        .get_cell_height = PBL_IF_ROUND_ELSE(get_cell_height_callback, NULL),
+      });
 
-  static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    window_set_background_color(report_window, GColorRajah);
+      // Bind the menu layer's click config provider to the window for interactivity
+      menu_layer_set_click_config_onto_window(menu_layer, window);
 
-  }
-
-  static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-    floodcount--;
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
-    text_layer_set_text(s_weather_layer, weather_layer_buffer);
-  }
-
-  static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    floodcount++;
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
-    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+      layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
     }
 
-  static void click_config_provider(void *context) {
-    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-  }
 
-  static void report_window_load(Window *window) {
+    static void main_window_unload(Window *window) {
+      text_layer_destroy(title_layer);
+      text_layer_destroy(region_layer);
+      menu_layer_destroy(menu_layer);
+    }
 
-  window_set_click_config_provider(window, (ClickConfigProvider) click_config_provider);
-    // Get information about the Window
-    Layer *window_layer = window_get_root_layer(window);
-    GRect bounds = layer_get_bounds(window_layer);
+    static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+      window_set_background_color(report_window, GColorRajah);
+
+    }
+
+    static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+      floodcount--;
+      snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
+      text_layer_set_text(s_weather_layer, weather_layer_buffer);
+    }
+
+    static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+      floodcount++;
+      snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
+      text_layer_set_text(s_weather_layer, weather_layer_buffer);
+    }
+
+    static void click_config_provider(void *context) {
+      window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+      window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+      window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+    }
+
+    static void report_window_load(Window *window) {
+
+      window_set_click_config_provider(window, (ClickConfigProvider) click_config_provider);
+      // Get information about the Window
+      Layer *window_layer = window_get_root_layer(window);
+      GRect bounds = layer_get_bounds(window_layer);
 
 
-    s_weather_layer = text_layer_create(
+      s_weather_layer = text_layer_create(
         GRect(0, 50, bounds.size.w, bounds.size.h));
 
-    // Style the text
-    text_layer_set_background_color(s_weather_layer, GColorClear);
-    text_layer_set_text_color(s_weather_layer, GColorBlack);
-    text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-    text_layer_set_overflow_mode(s_weather_layer, GTextOverflowModeWordWrap);
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
-    text_layer_set_text(s_weather_layer, weather_layer_buffer);
-    //text_layer_set_text(s_weather_layer, "Loading...");
-    text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+        // Style the text
+        text_layer_set_background_color(s_weather_layer, GColorClear);
+        text_layer_set_text_color(s_weather_layer, GColorBlack);
+        text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+        text_layer_set_overflow_mode(s_weather_layer, GTextOverflowModeWordWrap);
+        snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
+        text_layer_set_text(s_weather_layer, weather_layer_buffer);
+        //text_layer_set_text(s_weather_layer, "Loading...");
+        text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
 
 
-    // Create flood Layer
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
-  }
+        // Create flood Layer
+        layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+      }
 
-  static void report_window_unload(Window *window) {
-    layer_remove_child_layers(window_get_root_layer(window));
-    text_layer_destroy(s_weather_layer);
-}
+      static void report_window_unload(Window *window) {
+        layer_remove_child_layers(window_get_root_layer(window));
+        text_layer_destroy(s_weather_layer);
+      }
 
-static void contact_window_load(Window *window) {
-// Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+      static void contact_window_load(Window *window) {
+        // Get information about the Window
+        Layer *window_layer = window_get_root_layer(window);
+        GRect bounds = layer_get_bounds(window_layer);
 
-  contact_layer = text_layer_create(
-      GRect(0, 20, bounds.size.w, bounds.size.h));
+        contact_layer = text_layer_create(
+          GRect(0, 20, bounds.size.w, bounds.size.h));
 
-  // Style the text
-  text_layer_set_background_color(contact_layer, GColorClear);
-  text_layer_set_text_color(contact_layer, GColorWhite);
-  text_layer_set_text_alignment(contact_layer, GTextAlignmentCenter);
-  text_layer_set_overflow_mode(contact_layer, GTextOverflowModeWordWrap);
-  text_layer_set_text(contact_layer, "Emergency Call\n000");
-  text_layer_set_font(contact_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+          // Style the text
+          text_layer_set_background_color(contact_layer, GColorClear);
+          text_layer_set_text_color(contact_layer, GColorWhite);
+          text_layer_set_text_alignment(contact_layer, GTextAlignmentCenter);
+          text_layer_set_overflow_mode(contact_layer, GTextOverflowModeWordWrap);
+          text_layer_set_text(contact_layer, "Emergency Call\n000");
+          text_layer_set_font(contact_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
 
-  // Create flood Layer
-layer_add_child(window_get_root_layer(window), text_layer_get_layer(contact_layer));
-window_set_background_color(contact_window, GColorBlack);
-}
+          // Create flood Layer
+          layer_add_child(window_get_root_layer(window), text_layer_get_layer(contact_layer));
+          window_set_background_color(contact_window, GColorBlack);
+        }
 
-static void contact_window_unload(Window *window) {
-  layer_remove_child_layers(window_get_root_layer(window));
+        static void contact_window_unload(Window *window) {
+          layer_remove_child_layers(window_get_root_layer(window));
 
-  // Destroy weather elements
-  text_layer_destroy(contact_layer);
-}
-
-
-
-static void init(void) {
-
-  main_window = window_create();
-  window_set_background_color(main_window, GColorBlue);
-
-  window_set_window_handlers(main_window,(WindowHandlers){
-    .load = main_window_load,
-    .unload = main_window_unload});
+          // Destroy weather elements
+          text_layer_destroy(contact_layer);
+        }
 
 
-  report_window = window_create();
-  window_set_window_handlers(report_window, (WindowHandlers) {
-    .load = report_window_load,
-    .unload = report_window_unload
-  });
-  contact_window = window_create();
-  window_set_window_handlers(contact_window, (WindowHandlers) {
-    .load = contact_window_load,
-    .unload = contact_window_unload
-  });
-    window_stack_push(main_window, true);
 
-    app_message_register_inbox_received(inbox_received_callback);
-    app_message_register_inbox_dropped(inbox_dropped_callback);
-    app_message_register_outbox_failed(outbox_failed_callback);
-    app_message_register_outbox_sent(outbox_sent_callback);
+        static void init(void) {
 
-    const int inbox_size = 256;
-    const int outbox_size = 256;
-    app_message_open(inbox_size, outbox_size);
+          main_window = window_create();
+          window_set_background_color(main_window, GColorBlue);
+
+          window_set_window_handlers(main_window,(WindowHandlers){
+            .load = main_window_load,
+            .unload = main_window_unload});
 
 
-}
+            report_window = window_create();
+            window_set_window_handlers(report_window, (WindowHandlers) {
+              .load = report_window_load,
+              .unload = report_window_unload
+            });
+            contact_window = window_create();
+            window_set_window_handlers(contact_window, (WindowHandlers) {
+              .load = contact_window_load,
+              .unload = contact_window_unload
+            });
+            window_stack_push(main_window, true);
 
-static void deinit(void) {
-  //window_destroy(contact_window);
-  window_destroy(report_window);
-  window_destroy(main_window);
+            app_message_register_inbox_received(inbox_received_callback);
+            app_message_register_inbox_dropped(inbox_dropped_callback);
+            app_message_register_outbox_failed(outbox_failed_callback);
+            app_message_register_outbox_sent(outbox_sent_callback);
 
-}
+            const int inbox_size = 256;
+            const int outbox_size = 256;
+            app_message_open(inbox_size, outbox_size);
 
-int main(void) {
-  init();
-  app_event_loop();
-  deinit();
-}
+
+          }
+
+          static void deinit(void) {
+            //window_destroy(contact_window);
+            window_destroy(report_window);
+            window_destroy(main_window);
+
+          }
+
+          int main(void) {
+            init();
+            app_event_loop();
+            deinit();
+          }
