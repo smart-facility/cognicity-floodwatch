fb6f27175ce3f7435a925cd012424b029a82028c
Matthew Berryman <mberryma@uow.edu.au>
2016-03-10 17:05:18 +1100

    Two fixes: * string tokenisation issue, * bound checking on number of reports.

--- floodwatch-combined/src/floodwatch-combined.c
+++ floodwatch-combined/src/floodwatch-combined.c
@@ -11,6 +11,132 @@
 #define KEY_SOURCE 2
 
 
+/*
+ * we need to use a strdup and pebble don't have one, so:
+ */
+char *
+strdup(const char *s)
+{
+    char *p;
+
+    if (s == NULL)
+        return NULL;
+
+    if ((p = (char *)malloc(strlen(s)+1)) == NULL)
+        return NULL;
+
+    strcpy(p, s);
+
+    return p;
+}
+
+
+/* strtok licence, and strtok from http://www.opensource.apple.com/source/Libc/Libc-167/string.subproj/strtok.c
+ *
+ * Copyright (c) 1999 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.1 (the
+ * "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ *
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
+ */
+/*
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+ char *
+strtok(s, delim)
+	register char *s;
+	register const char *delim;
+{
+	register char *spanp;
+	register int c, sc;
+	char *tok;
+	static char *last;
+
+
+	if (s == NULL && (s = last) == NULL)
+		return (NULL);
+
+	/*
+	 * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
+	 */
+cont:
+	c = *s++;
+	for (spanp = (char *)delim; (sc = *spanp++) != 0;) {
+		if (c == sc)
+			goto cont;
+	}
+
+	if (c == 0) {		/* no non-delimiter characters */
+		last = NULL;
+		return (NULL);
+	}
+	tok = s - 1;
+
+	/*
+	 * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
+	 * Note that delim must have one NUL; we stop if we see that, too.
+	 */
+	for (;;) {
+		c = *s++;
+		spanp = (char *)delim;
+		do {
+			if ((sc = *spanp++) == c) {
+				if (c == 0)
+					s = NULL;
+				else
+					s[-1] = 0;
+				last = s;
+				return (tok);
+			}
+		} while (sc != 0);
+	}
+	/* NOTREACHED */
+}
+
 typedef struct {
   char *area;
   char *source;
@@ -34,15 +160,19 @@ static char weather_layer_buffer[32];
 static int current_icon = 0;
 static int floodcount = 0;
 
+static int min_token_count = 0;
+static int max_token_count = 0;
+
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
-  static char area_buffer[32];
-  static char source_buffer[32];
+  static char area_buffer[1024];
+  static char source_buffer[1024];
 
   char *area_temp;
   char *source_temp;
-  const char delim[2] = ",";
+  const char delim[] = ",";
   int token_count = 0;
+
 
   // Read tuples for data
   Tuple *area_tuple = dict_find(iterator, KEY_AREA);
@@ -63,14 +193,12 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
 
     /* get the first token */
     area_temp = strtok(area_buffer, delim);
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",area_temp);
 
     /* walk through other tokens */
     while( area_temp != NULL )
     {
-      flood_info[token_count].area= area_temp;
+      flood_info[token_count].area= strdup(area_temp);
       token_count++;
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",flood_info[token_count].area);
       area_temp = strtok(NULL, delim);
     }
 
@@ -80,12 +208,12 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     /* walk through other tokens */
     while( source_temp != NULL )
     {
-      flood_info[token_count].source= source_temp;
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "%s",flood_info[token_count].source);
+      flood_info[token_count].source= strdup(source_temp);
       token_count++;
       source_temp = strtok(NULL, delim);
     }
 
+    max_token_count = token_count-1;
   }
 }
 
@@ -243,12 +371,14 @@ static void main_window_load(Window *window) {
 
     static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
       floodcount--;
+      if (floodcount < min_token_count) floodcount = 0;
       snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
       text_layer_set_text(s_weather_layer, weather_layer_buffer);
     }
 
     static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
       floodcount++;
+      if (floodcount > max_token_count) floodcount = max_token_count;
       snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s\n%s\n%s",flood_info[floodcount].area,flood_info[floodcount].source,flood_info[floodcount].description);;
       text_layer_set_text(s_weather_layer, weather_layer_buffer);
     }
