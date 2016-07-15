a55affdf1f32a840c2197a61beb34ff54caeea61
Tomas <tomasholderness@gmail.com>
2016-07-15 08:35:35 +0100

    removed old windows modules

--- floodwatch-combined/src/windows/contact.h
+++
@@ -1,5 +0,0 @@
-/*#pragma once
-
-#include <pebble.h>
-
-void contact_window_push()*/
--- floodwatch-combined/src/windows/main_window.h
+++
@@ -1,6 +0,0 @@
-/*#ifndef MAIN_WINDOW_LOAD_H
-#define MAIN_WINDOW_LOAD_H
-
-static void main_window_load(Window *window);
-
-#endif*/
