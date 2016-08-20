70ec59631cc8a3b35345ec4c9e222be6380fdb97
Tomas <tomasholderness@gmail.com>
2016-08-20 15:41:50 +0100

    Fixes #10 - max number of reports in location filter

--- floodwatch/src/js/app.js
+++ floodwatch/src/js/app.js
@@ -9,21 +9,29 @@ var turf_distance = require('./libs/@turf/distance');
 // Get user location
 var getUserLocation = function(callback){
 
+  var watch_location = {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates" : []
+      }
+    };
+
+  var watch_location_status = 1;
+
   function success(pos){
-    var user_location = {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "type": "Point",
-          "coordinates": [pos.coords.longitude, pos.coords.latitude]
-          //"coordinates":[106.826355,-6.1763649] /*for testing - @MONAS*/
-        }
-    }
-    callback(user_location);
+    //watch_location.geometry.coordinates.push(106.826355,-6.1763649); // MONAS
+    //watch_location.geometry.coordinates.push(106.78839683532715,-6.176224966990518); // Tanjung Duren Utara
+    watch_location.geometry.coordinates.push(pos.coords.longitude, pos.coords.latitude);
+
+    watch_location_status = 0;
+    callback(watch_location, watch_location_status);
   }
 
-  function error(err){
-    callback(err);
+  function error(){
+    watch_location_status = 1;
+    callback(watch_location, watch_location_status);
   }
 
   var options = {
@@ -31,63 +39,67 @@ var getUserLocation = function(callback){
     maximumAge: 10000,
     timeout: 10000
   };
-
   navigator.geolocation.getCurrentPosition(success, error, options);
 }
 
 // Process incoming flood data from PetaJakarta.org
-var processReports = function(report_string, status){
+var processReports = function(report_string, report_status){
 
   // Data stores
-  var pkey = [];
-  var text = ['[Success] No nearby flood reports'];
-  var time = [];
-  var distance = [];
+  var reports_dictionary = {
+    pkey : [],
+    text : ['[Success] No nearby flood reports'],
+    time : [],
+    distance : []
+  };
 
-  getUserLocation(function(user_location){
-
-    if (status !== 200){
-      console.log('Error communicating with server ('+status+')');
-      text[0] = '[Error '+status+'] Problem communicating with server';
+  getUserLocation(function(watch_location, watch_location_status){
+    if (report_status !== 200){
+      console.log('Error communicating with server ('+report_status+')');
+      reports_dictionary.text[0] = '[Error '+report_status+'] Problem communicating with server';
+    }
+    else if (watch_location_status !== 0) {
+      console.log('Error getting watch location');
+      reports_dictionary.text[0] = '[Error] Could not detemine\nwatch location';
     }
     else {
+      // Filter by location
       var reports = JSON.parse(report_string);
-      // check for user location
-      if (user_location.type == "Feature"){
-        // check that there are some reports
-        if (reports.features !== null) {
-          // loop reports and check if nearby
-          for (var i = 0; i < Math.min(reports.features.length, 10); i++){
-            var dist = turf_distance(user_location, reports.features[i], 'kilometers');
-            if (dist <= 5.0) {
-              dist = dist.toFixed(1);
-              pkey[i] = reports.features[i].properties.pkey;
-              if (reports.features[i].properties.text.length > 160){
-                text[i] = reports.features[i].properties.text.slice(0,156)+'...'
-              }
-              else {
-                text[i] = reports.features[i].properties.text.slice(0,160);
-              }
-              time[i] = reports.features[i].properties.created_at.slice(11,16);
-              distance[i] = dist;
+      if (reports.features !== null){
+        var j = 0; // dictionary counter
+        for (var i = 0; i < reports.features.length; i++){
+
+          var dist = turf_distance(watch_location, reports.features[i], 'kilometers');
+          if (dist <= 5.0 && reports_dictionary.pkey.length < 10) {
+
+            dist = dist.toFixed(1); //Round to 1DP
+            reports_dictionary.distance[j] = dist;
+
+            reports_dictionary.pkey[j] = reports.features[i].properties.pkey;
+            if (reports.features[i].properties.text.length > 160){
+              // Add ellipsis to long text strings
+              reports_dictionary.text[j] = reports.features[i].properties.text.slice(0,156)+'...';
             }
+            else {
+              reports_dictionary.text[j] = reports.features[i].properties.text.slice(0,160);
+            }
+            reports_dictionary.time[j] = reports.features[i].properties.created_at.slice(11,16);
+            j += 1 // increment dictionary counter
           }
         }
       }
-      else {
-        text[0] = '[Error] Could not detemine user location';
-      }
     }
+
     // Assemble dictionary using our keys
-    var dictionary = {
-      "KEY_PKEY": pkey.toString(),
-      "KEY_DISTANCE":distance.toString(),
-      "KEY_TIME": time.toString(),
-      "KEY_DESCRIPTION": text.join("|"),
-      "KEY_DATA_LENGTH": (pkey.length).toString()
+    var pebble_dictionary = {
+      "KEY_PKEY": reports_dictionary.pkey.toString(),
+      "KEY_DISTANCE":reports_dictionary.distance.toString(),
+      "KEY_TIME": reports_dictionary.time.toString(),
+      "KEY_DESCRIPTION": reports_dictionary.text.join("|"),
+      "KEY_DATA_LENGTH": (reports_dictionary.pkey.length).toString()
     };
 
-    Pebble.sendAppMessage(dictionary,
+    Pebble.sendAppMessage(pebble_dictionary,
       function(e) {
         console.log("Flood info sent to Pebble successfully!");
       },
@@ -114,7 +126,7 @@ Pebble.addEventListener('ready',
     console.log("PebbleKit JS ready!");
     // Request flood reports from API
     var url = "https://petajakarta.org/banjir/data/api/v2/reports/confirmed";
-    //var url = "http://127.0.0.1:8080/sample_reports.json"
+    //var url = "http://localhost:8080/sample_reports.json"
     xhrRequest(url, 'GET', processReports);
     }
 );
