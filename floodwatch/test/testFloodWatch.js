71b3461663bee7ac94a306b13485d6d4f4c65692
Tomas <tomasholderness@gmail.com>
2016-08-21 09:56:32 +0100

    Added tests for processing of report data

--- floodwatch/test/testFloodWatch.js
+++ floodwatch/test/testFloodWatch.js
@@ -3,8 +3,9 @@
 var test = require('unit.js');
 var fs = require('fs');
 // Reports JSON
-//var report_data = fs.readFileSync('./test/sample_reports.json');
+var eleven_reports = fs.readFileSync('./test/sample_reports.json');
 var single_report = fs.readFileSync('./test/one_sample_report.json');
+var faraway_report = fs.readFileSync('./test/one_faraway_sample_report.json');
 // Spoof Pebble navigator function
 global.navigator = {
   userAgent: 'node.js',
@@ -87,6 +88,27 @@ describe("processReport data handling and filtering", function(){
   it ('Finds one report within 5 km of the watch location', function(){
     FloodWatch.processReports(single_report, 200, function(data){
       test.value( data.KEY_PKEY ).is("1");
+    });
+  });
+  it ('Truncates description longer than 160 chars', function(){
+    FloodWatch.processReports(single_report, 200, function(data){
+      test.value( data.KEY_DESCRIPTION).is("1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla odio elit, semper nec purus et, facilisis dignissim arcu. In urna ante, sollicitudin ut sod...");
+    });
+  });
+  it ('Truncates timestamps to HH:MM', function(){
+    FloodWatch.processReports(single_report, 200, function(data){
+      test.value( data.KEY_TIME).is("13:16");
+    });
+  });
+  it ('Only returns a maximum of 10 reports', function(){
+    FloodWatch.processReports(eleven_reports, 200,  function(data){
+      test.value( JSON.parse(eleven_reports).features.length).is(11);
+      test.value( data.KEY_DATA_LENGTH).is("10");
+    });
+  });
+  it('Excludes reports which are more than 5 km away', function(){
+    FloodWatch.processReports(faraway_report, 200, function(data){
+      test.value( data.KEY_DESCRIPTION).is("[Success] No nearby flood reports");
     });
   });
   // Return location function to normal
