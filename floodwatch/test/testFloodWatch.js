b80ec69692dea0457685f7dcb195cf46174fce78
Tomas <tomasholderness@gmail.com>
2016-08-21 09:42:47 +0100

    Added test for processingvalid report data

--- floodwatch/test/testFloodWatch.js
+++ floodwatch/test/testFloodWatch.js
@@ -1,16 +1,29 @@
 'use strict';
-
+// Required modules
 var test = require('unit.js');
-
+var fs = require('fs');
+// Reports JSON
+//var report_data = fs.readFileSync('./test/sample_reports.json');
+var single_report = fs.readFileSync('./test/one_sample_report.json');
+// Spoof Pebble navigator function
 global.navigator = {
   userAgent: 'node.js',
   geolocation:{
     getCurrentPosition: function(success, error, location){
-      console.log('spoof');
+      return(0);
     }
   }
 };
 var FloodWatch = require('../src/js/floodwatch.js');
+
+var location = {
+   "type": "Feature",
+   "properties": {},
+   "geometry": {
+     "type": "Point",
+     "coordinates" : [106.826355,-6.1763649] //MONAS
+   }
+};
 
 describe("processReports error status handling", function(){
 
@@ -61,6 +74,23 @@ describe("processReports null data handling", function(){
   });
 });
 
-//describe("processReport data handling and filtering", function(){
-
-//})
+describe("processReport data handling and filtering", function(){
+  var oldGetLocation;
+  // Mock location function with real data
+  before( function(){
+    oldGetLocation = FloodWatch.getLocation;
+    FloodWatch.getLocation = function(callback){
+      callback(location, 0);
+    };
+  });
+  // Test real data handling
+  it ('Finds one report within 5 km of the watch location', function(){
+    FloodWatch.processReports(single_report, 200, function(data){
+      test.value( data.KEY_PKEY ).is("1");
+    });
+  });
+  // Return location function to normal
+  after ( function(){
+    FloodWatch.getLocation = oldGetLocation;
+  });
+});
