a6441221c52c31907a3953c8cfa7c2aa94a63dce
Tomas <tomasholderness@gmail.com>
2016-08-21 09:07:43 +0100

    Added tests for processReports error handling

--- floodwatch/test/testFloodWatch.js
+++ floodwatch/test/testFloodWatch.js
@@ -12,13 +12,51 @@ global.navigator = {
 };
 var FloodWatch = require('../src/js/floodwatch.js');
 
-describe("processReports", function(){
-  it( 'Catches error value in reports data != 200', function(){
-    FloodWatch.processReports('test', 0, function(data){
-      text.value( data.KEY_DESCRIPTION ).is('[Error 0] Problem communicating with server');
+describe("processReports error status handling", function(){
+
+  var oldGetLocation;
+  // Mock location function
+  before( function(){
+    oldGetLocation = FloodWatch.getLocation;
+    FloodWatch.getLocation = function(callback){
+      callback('location', 1);
+    }
+  });
+  // Test reports status handling
+  it( 'Catches error value in reports data status != 200', function(){
+    FloodWatch.processReports('test', 404, function(data){
+      test.value( data.KEY_DESCRIPTION ).is('[Error 404] Problem communicating with server');
     });
   });
-  /*it ('Catches error value in watch location data != 0', function(){
-    FloodWatch.processReports
-  })*/
+  // Test location status handling
+  it ('Catches error value in watch location data != 0', function(){
+    FloodWatch.processReports('test', 200, function(data){
+      test.value( data.KEY_DESCRIPTION ).is('[Error] Could not detemine\nwatch location');
+    })
+  });
+  // Return location function to normal
+  after ( function(){
+    FloodWatch.getLocation = oldGetLocation;
+  });
+});
+
+describe("processReports null data handling", function(){
+  var oldGetLocation;
+  // Mock location function
+  before( function(){
+    oldGetLocation = FloodWatch.getLocation;
+    FloodWatch.getLocation = function(callback){
+      callback('location', 1);
+    }
+  });
+  // Test null data handling
+  it ('Catches empty reports data', function(){
+    FloodWatch.processReports('{"features": null}', 200, function(data){
+      test.value( data.KEY_DESCRIPTION ).is('[Error] Could not detemine\nwatch location');
+    })
+  });
+  // Return location function to normal
+  after ( function(){
+    FloodWatch.getLocation = oldGetLocation;
+  });
 });
