c2dd5846b915e816d2845902305b21e4a4e1a1d8
Tomas <tomasholderness@gmail.com>
2016-08-25 09:19:09 +0200

    Fixes #12 - checks for BT connectivity

--- floodwatch/src/main.c
+++ floodwatch/src/main.c
@@ -14,25 +14,25 @@ static char message_buffer[50];
 
 // Initialise windows
 extern void init_windows(void) {
-
-  splash_window_push();
-
+  splash_window_push(); // Load splash window
+  // Prepare for data
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
-
-  bool app_connection = connection_service_peek_pebble_app_connection();
-  if (app_connection == 1){
-     app_message_open(INBOX_SIZE, OUTBOX_SIZE);
+  // Check Bluetooth connectivity
+  // *TO DO* there is latency in bluetooth peek function from Pebble
+  if (connection_service_peek_pebble_app_connection()){
+    app_message_open(INBOX_SIZE, OUTBOX_SIZE);
   }
   else {
-    snprintf(message_buffer, sizeof(message_buffer), "%s", "[Error] No bluetooth connection available");
-    splash_window_pull();
-    message_window_push(message_buffer);
+     snprintf(message_buffer, sizeof(message_buffer), "%s", "[Error] No bluetooth connection available");
+     splash_window_pull();
+     message_window_push(message_buffer);
   }
 }
 
 // Deinitialise windows
 extern void deinit_windows(void) {
+  // *TO DO* need to pull all windows / check what loaded?
   splash_window_pull();
 }
 
